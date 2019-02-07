970308bd6d1d0fa1a06621e7713a4f7a6d51c8ac
Morten Bujordet <mo.bujordet@gmail.com>
2019-02-07 15:18:52 +0100

    Fix ignorefiles and update cors

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
-from flask_restful import Api, Resource
+from flask_cors import CORS
+from flask_restful import Api
 from resources.Hello import Hello
 
 api_bp = Blueprint('api', __name__)
+cors = CORS(api_bp, resources={r"*": {"origins": "*"}})
 api = Api(api_bp)
 
 # Route
