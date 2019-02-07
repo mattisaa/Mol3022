2298d0e9dc860c0b81477415a6fc9d459a98eeaf
Morten Bujordet <mo.bujordet@gmail.com>
2019-02-07 11:37:10 +0100

    Add api functions

---
+++ frontend/src/config.js
@@ -0,0 +1,4 @@
+export const API_URL = process.env.API_URL || 'http://localhost:8000';
+export const BASE_HEADERS = {
+  'Content-Type': 'application/json'
+};
---
+++ frontend/src/utils/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { API_URL, BASE_HEADERS } from '../config';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: BASE_HEADERS
+});
+
+export function get(url) {
+  return api
+    .get(`${url}/`)
+    .then(res => res)
+    .catch(err => err.response);
+}
+
+export function put(url, data) {
+  return api
+    .put(`${url}/`, data)
+    .then(res => res)
+    .catch(err => err.response);
+}
+
+export function post(url, body) {
+  return api
+    .post(`${url}/`, body)
+    .then(res => res)
+    .catch(err => err.response);
+}
+
+export function remove(url) {
+  return api
+    .delete(`${url}/`)
+    .then(res => res)
+    .catch(err => err.response);
+}
