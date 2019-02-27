2e3d5aff81137666c83c0818b6b5ff341355ac60
mattisaa <mattis_araya@hotmail.com>
2019-02-27 13:17:15 +0100

    add list of bedtools with corresponding name

--- backend/resources/Bedtools.py
+++ backend/resources/Bedtools.py
@@ -5,7 +5,11 @@ from data.data import get_data
 class Bedtools(Resource):
 
     def get(self):
+        list_of_names = ['exons', 'cpg']
         list_of_bedtools = get_data()
+        list_of_Bedtoolstuples = []
+        for i in range(len(list_of_bedtools)):
+            list_of_tuples.append((list_of_bedtools[i], list_of_names[i]))
         res = list_of_bedtools[0].jaccard(list_of_bedtools[1])
         print(res)
         return str(res)
