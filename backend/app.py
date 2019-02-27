74f31857d10a4b36df1dcb91b2633a8a609c57cd
Morten Bujordet <mo.bujordet@gmail.com>
2019-02-27 11:56:41 +0100

    Add fix

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,11 @@
 from flask import Blueprint
 from flask_cors import CORS
 from flask_restful import Api
-from resources.Hello import Hello
 from resources.Bedtools import Bedtools
 
 api_bp = Blueprint('api', __name__)
-cors = CORS(api_bp, resources={r"*": {"origins": "*"}})
+CORS(api_bp)
 api = Api(api_bp)
 
 # Route
-api.add_resource(Hello, '/')
-api.add_resource(Bedtools, '/bedtools')
+api.add_resource(Bedtools, '/')
--- backend/resources/Bedtools.py
+++ backend/resources/Bedtools.py
@@ -1,11 +1,10 @@
 from flask_restful import Resource
+from flask import jsonify
 import pybedtools
 
 class Bedtools(Resource):
-
     def get(self):
         a = pybedtools.example_bedtool('a.bed')
         b = pybedtools.example_bedtool('b.bed')
-        res = a.intersect(b)
-        print(res)
-        return str(res)
+        res = a.jaccard(b)
+        return jsonify(res)
