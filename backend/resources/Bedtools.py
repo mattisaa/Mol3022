b31e50f2e2dfa6b8926e9c54bc9f9324c984e700
mattisaa <mattis_araya@hotmail.com>
2019-02-28 11:26:18 +0100

    Add more genomes to genomes.py

--- backend/resources/Bedtools.py
+++ backend/resources/Bedtools.py
@@ -5,11 +5,15 @@ from data.data import get_data
 class Bedtools(Resource):
 
     def get(self):
-        list_of_names = ['exons', 'cpg']
+        list_of_names = ['exons', 'cpg', 'fStomach-DS17659', 'fSkin_fibro_bicep_R-DS19745', 'fKidney_renal_cortex_L-DS17550', 'fLung_R-DS15632']
         list_of_bedtools = get_data()
         list_of_Bedtoolstuples = []
         for i in range(len(list_of_bedtools)):
-            list_of_tuples.append((list_of_bedtools[i], list_of_names[i]))
-        res = list_of_bedtools[0].jaccard(list_of_bedtools[1])
-        print(res)
-        return str(res)
+            list_of_Bedtoolstuples.append((list_of_bedtools[i], list_of_names[i]))
+        print(list_of_bedtools)
+        res = list_of_bedtools[0].jaccard(list_of_bedtools[2])
+        sorted = list_of_bedtools[1].sort()
+        random_shuffle = sorted.shuffle(genome='hg19', chrom=True, seed=1)
+        res2 = list_of_bedtools[0].jaccard(random_shuffle.sort())
+        print(res2)
+        return str(res), str(res2)
