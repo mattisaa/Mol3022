93e77e125c39a9da6455862570e310e07f1cd449
Morten Bujordet <mo.bujordet@gmail.com>
2019-02-28 11:08:29 +0100

    Feat/endpoint rename endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,11 @@
 from flask import Blueprint
 from flask_cors import CORS
 from flask_restful import Api
-from resources.Hello import Hello
-from resources.Bedtools import Bedtools
+from resources.Genomes import Genomes
 
 api_bp = Blueprint('api', __name__)
 cors = CORS(api_bp, resources={r"*": {"origins": "*"}})
 api = Api(api_bp)
 
 # Route
-api.add_resource(Hello, '/')
-api.add_resource(Bedtools, '/bedtools')
+api.add_resource(Genomes, '/genomes')
--- backend/resources/Genomes.py
+++ backend/resources/Genomes.py
@@ -1,8 +1,11 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from flask import jsonify
 import pybedtools
 from data.data import get_data
 
-class Bedtools(Resource):
+parser = reqparse.RequestParser()
+
+class Genomes(Resource):
 
     def get(self):
         list_of_names = ['exons', 'cpg', 'fStomach-DS17659', 'fSkin_fibro_bicep_R-DS19745', 'fKidney_renal_cortex_L-DS17550', 'fLung_R-DS15632']
@@ -17,3 +20,10 @@ class Bedtools(Resource):
         res2 = list_of_bedtools[0].jaccard(random_shuffle.sort())
         print(res2)
         return str(res), str(res2)
+
+    def post(self):
+        parser.add_argument('test', type=str)
+        args = parser.parse_args()
+        print(args)
+        test = {"Morten": "Bujordet"}
+        return "Morten", 200
